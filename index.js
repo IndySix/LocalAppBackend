98d74cdf4ceb2dc5922e4eeea69432d6ef415da6
Joost Plattel <jsplattel@gmail.com>
2013-10-30 11:33:05 +0100

    Added two more API calls

--- index.js
+++ index.js
@@ -11,17 +11,22 @@ var rfidReaderPath = "/dev/tty.usbmodem641"
 var sensorReaderPath = "/dev/tty.usbmodem441"
 
 // Global variables 
-var API_URL = 'http://145.89.128.41/projecten/website/api/';
-var RFID_KEY = '';
-var USER_DATA = ''
+var API_URL = 'http://145.89.128.48/projecten/website/api/';
+RFID_KEY = '';
+var USER_DATA = '{ "status" : "fail", "data" : {} }'
+
+
 
 // Local server setup for API
 var express = require('express'); // Framework 
-var web = express(); // Setup app
-web.use(express.bodyParser()); // Configure POST parser
+var web = express(); // Setup app and configure:
+web.use(express.json());
+web.use(express.urlencoded());
 
 // GET user data and return it for the bigger view
-web.get('/user', function(req, res) {
+web.get('/checkinUser', function(req, res) {
+
+	// console.log(RFID_KEY)
 
 	// If RFID is present, get the user based on the RFID KEY
 	if (RFID_KEY){
@@ -30,11 +35,12 @@ web.get('/user', function(req, res) {
 		request(r, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				USER_DATA = body; // Set user data for returning 
+
 			} 
 		})
 	}
 
-	console.log(USER_DATA); // Contains everything we know about the current checked in User
+	// console.log(USER_DATA); // Contains everything we know about the current checked in User
 
 	// Build JSON response
 	res.header("Access-Control-Allow-Origin", "*");
@@ -42,8 +48,49 @@ web.get('/user', function(req, res) {
 	res.writeHead(200, { 'Content-Type': 'application/json' }); 
 	res.write(USER_DATA);
 	res.end()
-
 });
+
+
+web.get('/latestResultsParts/grind', function(req, res) {	
+	r = API_URL + '/latestResultsParts/grind'
+	
+	var d = '{ "status" : "fail", "data" : {} }'
+	request(r, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			d = body; // Set user data for returning 
+		} 
+	})
+
+	// Build JSON response
+	res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With"); 
+	res.writeHead(200, { 'Content-Type': 'application/json' }); 
+	res.write(d);
+	res.end()
+});
+
+web.get('/getKingPart/grind', function(req, res) {	
+	r = API_URL + '/getKingPart/grind'
+	
+	var d = '{ "status" : "fail", "data" : {} }'
+	request(r, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			d = body; // Set user data for returning 
+		} 
+	})
+
+	// Build JSON response
+	res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With"); 
+	res.writeHead(200, { 'Content-Type': 'application/json' }); 
+	res.write(d);
+	res.end()
+});
+
+
+
+
+
 
 // TODO: Serve other pages here too... 
 // Local port for listing to requests
@@ -54,80 +101,35 @@ console.log('Serving pages on: http://localhost:8000')
 var rfidreader = new SerialPort(rfidReaderPath, { baudrate: 9600 , parser: serialport.parsers.readline("\r\n")});
 if (rfidreader){
 	rfidreader.on("open", function () {
-	  	rfidreader.on('data', function(data) {	 		
+	  	rfidreader.on('data', function(data) {	
  			var key = data;
- 			console.log("Starting challenge for: " + key);
- 			setTimeout(function() { finishChallenge(key) }, 5000)
- 			RFID_KEY = key; // Set RFID key globally 
+ 			RFID_KEY = key;	
+ 			console.log("Starting challenge for: " + RFID_KEY);
+ 			setTimeout(function() { finishChallenge(RFID_KEY) }, 20000)
 	  	});  
 	});
 }
 
 function finishChallenge(key){
 	console.log('Finished challenge for: ' + key)
+	RFID_KEY = ''
+	USER_DATA = '{ "status" : "failure", "data" : {} }'
 }
 
 // Setup serial port for sensors.
-var direction = 'R'
-var oldvalues = new Array(2);
-var start_record_time = 0;
-var start_detect_time = 0;
-
 var sensorreader = new SerialPort(sensorReaderPath, { baudrate: 9600 , parser: serialport.parsers.readline("\r\n") });
 if (sensorreader){
-
 	sensorreader.on("open", function () {
-	  	sensorreader.on('data', function(d) {
-	 		
-	  			// console.log(d);
-		 		var newvalues = d.split(':'); // Parse this until a line ending character is found
-
-		 		var left_old_dist = parseInt(oldvalues[1]);
-		 		var left_new_dist = parseInt(newvalues[1]);
-				var right_old_dist = parseInt(oldvalues[2]);
-		 		var right_new_dist = parseInt(newvalues[2]);
-
-		 		var time_dif = parseInt(newvalues[0]) - start_record_time;
-		 		var detect_time_dif = parseInt(newvalues[0]) - start_detect_time;
-
-		 		oldvalues = newvalues; // Cast new values to the old ones for the next loop
-
-		 		if (time_dif > start_record_time){
-			 		if (direction = 'R'){
-			 			if(right_new_dist < right_old_dist && right_old_dist - right_new_dist > 40 && detect_time_dif > 250) {
-							detect_time_dif = parseInt(newvalues[0]);
-						}
-
-			 			if(right_new_dist < right_old_dist && right_old_dist - right_new_dist > 40 && detect_time_dif < 250) {
-							console.log("Skater passed right");
-							start_record_time = parseInt(newvalues[1]);
-							// Start movie here
-							// Spawn python script for recording video.
-					 		//spawn('python', ['/pathtoscript', '-c', 'camerapath', '-n', 'filename', '-d', 'duration']);
-						}
-
-						
-			 		} 
-
-					if (direction = 'L'){
-						if(left_new_dist < left_old_dist && left_old_dist - left_new_dist > 40 && detect_time_dif > 250) {
-							detect_time_dif = parseInt(newvalues[0]);
-						}
-
-			 			if(right_new_dist < right_old_dist && right_old_dist-right_new_dist > 40 && detect_time_dif < 250) {
-							console.log("Skater passed left");
-							start_record_time = parseInt(newvalues[1]);
-							// Start movie here
-							// Spawn python script for recording video.
-					 		//spawn('python', ['/pathtoscript', '-c', 'camerapath', '-n', 'filename', '-d', 'duration']);
-						}
-						
-			 		} 
-				}
-	  		
+	  	sensorreader.on('data', function(data) {
+  		
+  			if (data != "N"){
+  				console.log(data)
+	 			// Start movie here
+	            // Spawn python script for recording video.
+	            //spawn('python', ['/pathtoscript', '-c', 'camerapath', '-n', 'filename', '-d', 'duration']);
+  			}
 	  	});  
 	});
-}
-
+} 
 
 
